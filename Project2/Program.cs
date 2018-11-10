314c6ed77b997a645bfe06c30978b1a87750879f
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2018-11-10 18:37:54 +0200

    Final_build

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,21 +1,39 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Project2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string end;
+            do
+            {
+                var x = new Results();
+                x.Calculate(x.Symbol);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                var y = new Results();
+                y.Calculate(y.Symbol);
+
+                //checking and printing The Greater commong Divison of 2 results.
+                Console.WriteLine($"The Greater Common Divisor Between {x.result} and {y.result} is {Results.GCD((int)x.result, (int)y.result)}");
+                //check if Result 2 is a Prime number
+                if (Results.IsPrime((int)y.result))
+                {
+                    Console.WriteLine($"The number {y.result} is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {y.result} is not a prime number");
+                }
+                //Finding Fibonacci of result2
+                Console.WriteLine($"The Fibonacci number of {y.result} is {Results.Fibonacci((int)y.result)}");
+                Console.WriteLine("Do u want to Calculate something more? y/n");
+                end = Console.ReadLine();               
+            } while (end != "n");
+            Console.WriteLine("Bye Bye");
+            Console.ReadKey();
         }
     }
 }
---
+++ Project2/Results.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class Results
+    {
+        private double Number1 { get; set; }
+        private double Number2 { get; set; }
+        public string Symbol { get; set; }
+        public double result { get; set; }
+
+        public Results()
+        {
+            Console.WriteLine("Give me your 1st number");
+            Number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Give me your 2nd number");
+            Number2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Give me a symbol between :  +, -, *, /, %, square root");
+                Symbol = Console.ReadLine();
+            } while (Symbol != "+" && Symbol != "-" && Symbol != "*" && Symbol != "/" && Symbol != "%" && Symbol != "square root");
+        }
+
+        public void Calculate(string symbol)
+        {
+            switch (symbol)
+            {
+                default:
+                case "+":
+                    result = Number1 + Number2;
+                    Console.WriteLine($"The result is: {Number1} + {Number2} = {result}");
+                    break;
+                case "-":
+                    result = Number1 - Number2;
+                    Console.WriteLine($"The result is: {Number1} - {Number2} = {result}");
+                    break;
+                case "*":
+                    result = Number1 * Number2;
+                    Console.WriteLine($"The result is: {Number1} * {Number2} = {result}");
+                    break;
+                case "/":
+                    while (Number2 == 0)
+                    {
+                        Console.WriteLine("Give me a Number != 0");
+                        Number2 = double.Parse(Console.ReadLine());
+                    }
+                    result = Number1 / Number2;
+                    Console.WriteLine($"The result is: {Number1} / {Number2} = {result}");
+                    break;
+                case "%":
+                    while (Number2 == 0)
+                    {
+                        Console.WriteLine("Give me a Number != 0");
+                        Number2 = double.Parse(Console.ReadLine());
+                    }
+                    result = Number1 % Number2;
+                    Console.WriteLine($"The result is: {Number1} % {Number2} = {result}");
+                    break;
+                case "square root":
+                    //TODO:
+                    double result1 = Number1 - Number2;
+                    if (result1 <= 0)
+                        result = 0;
+                    else
+                        result = Math.Sqrt(Number1) * Math.Sqrt(Number2);
+                    Console.WriteLine($"The result is: {Number1} square root {Number2} = {result}");
+                    break;
+            }
+        }
+
+
+        //public static ulong GCD(ulong a, ulong b)
+        //{
+        //    while (a != 0 && b != 0)
+        //    {
+        //        if (a > b)
+        //            a %= b;
+        //        else
+        //            b %= a;
+        //    }
+
+        //    return a == 0 ? b : a;
+        //
+        public static int GCD(int p, int q)
+        {
+            if (q == 0)
+            {
+                return p;
+            }
+
+            int r = p % q;
+
+            return GCD(q, r);
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
